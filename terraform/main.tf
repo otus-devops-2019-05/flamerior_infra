eca72d4d4fd3a4342fd52b459af5f7f1dc981973
anechaev <anechaev@plesk.com>
2019-07-01 16:57:30 +0700

    homework №6 added load balancer

---
+++ terraform/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  # Версия terraform
+  required_version = "0.11.13"
+}
+
+provider "google" {
+  # Версия провайдера
+  version = "2.0.0"
+
+  # ID проекта
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_compute_project_metadata_item" "appuser1ssh" {
+  key   = "ssh-keys"
+  value = "appuser:${file(var.public_key_path)}\nappuser1:${file(var.public_key_path)}\nappuser2:${file(var.public_key_path)}\nappuser3:${file(var.public_key_path)}"
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app-${count.index}"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  count        = "${var.app_quantity}"
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  //  metadata {
+  //    ssh-keys = "appuser:${file(var.public_key_path)}"
+  //  }
+
+  tags = ["reddit-app"]
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
